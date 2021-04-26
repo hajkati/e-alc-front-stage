d8cd67b96250996b7ab447da196eb5ddb59832d9
mohcinemourchid99 <mohcine.mourchid@edu.uca.ma>
2021-04-26 23:41:19 +0000

    commit

--- src/app/controller/model/question.model.ts
+++ src/app/controller/model/question.model.ts
@@ -1,28 +1,11 @@
-import {Reponse} from './reponse.model';
-import {TypeDeQuestion} from './type-de-question.model';
-import {Quiz} from './quiz.model';
 
 export class Question {
-  // tslint:disable-next-line:variable-name
-  // @ts-ignore
-  // tslint:disable-next-line:variable-name
-  // @ts-ignore
-  // @ts-ignore
-  public id: number;
-  // @ts-
-  // @ts-ignore
-  public numero: number;
-  // @ts-ignore
-  public ref: string;
-  // @ts-ignore
-  public libelle: string;
-  // @ts-ignore
-  public pointReponseJuste: number;
-  // @ts-ignore
-  public pointReponsefausse: number;
-  // @ts-ignore
-  public type = new TypeDeQuestion();
-  public reponses = new Array<Reponse>();
-  public quiz = new Quiz();
-  // @ts-ignore
+  public id : number;
+  public ref : string;
+  public libelle : string;
+  public numero : number;
+  public pointReponseJuste : number;
+  public pointReponsefausse : number;
+  //public typeDeQuestion = new TypeDeQuestion();
+ // public quiz = new Quiz();
 }
